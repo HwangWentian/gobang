1664f5143ee2e7e62d3d2a0b77641e9498780960
黄闻天 HwangWentian <85811921+HwangWentian@users.noreply.github.com>
2022-12-25 14:07:48 +0800

    A BIG CHANGE!
    Added the ability to judge winners and a pocket version

--- 五子棋 袖珍版.py
+++ 五子棋 袖珍版.py
@@ -1,20 +1,17 @@
 def drawChessboard() -> list:
     board = []
-    column = "     "
+    column = "   "
+    for i in range(1, 16):
+        column += chr(64 + i) + " "
+    board.append(column)
     row = ""
     for i in range(1, 16):
-        column += chr(64 + i) + "   "
-    board.append(column)
-    for i in range(1, 16):
-        row = "   " + "+---" * 15 + "+"
-        board.append(row)
         if i < 10:
-            row = "0" + str(i)
+            row = "0" + str(i) + " "
         else:
-            row = str(i)
-        row += " |  " * 15 + " | " + row
+            row = str(i) + " "
+        row += ". " * 14 + "." + " " + row
         board.append(row)
-    board.append("   " + "+---" * 15 + "+")
     board.append(column)
     return board
 
@@ -37,21 +34,21 @@ def inputCoordinate() -> tuple:
             break
         except:
             print("输入错误")
-    return 4 * ord(x) - 255, 2 * y
+    return 2 * ord(x) - 127, y
 
 
 def move(board: list, handle: bool) -> tuple:
-    if handle:
+    if (handle):
         print("黑方行")
     else:
         print("白方行")
     while (True):
         x, y = inputCoordinate()
-        if (board[y][x] == " "):
-            if handle:
+        if (board[y][x] == "."):
+            if (handle):
                 board[y] = board[y][:x] + "@" + board[y][x + 1:]
             else:
-                board[y] = board[y][:x] + "#" + board[y][x + 1:]
+                board[y] = board[y][:x] + "O" + board[y][x + 1:]
             break
         else:
             print("此位置已有棋")
@@ -60,38 +57,38 @@ def move(board: list, handle: bool) -> tuple:
 
 def getRows(x: int, y: int, board: list) -> list:
     rows = []
-    if x >= 19:
-        rows.append(board[y][x - 4] + board[y][x - 8] +
-                    board[y][x - 12] + board[y][x - 16])
-    if x <= 43:
-        rows.append(board[y][x + 4] + board[y][x + 8] +
-                    board[y][x + 12] + board[y][x + 16])
-    if y >= 9:
-        rows.append(board[y - 2][x] + board[y - 4][x] +
-                    board[y - 6][x] + board[y - 8][x])
-    if y <= 21:
-        rows.append(board[y + 2][x] + board[y + 4][x] +
-                    board[y + 6][x] + board[y + 8][x])
-    if x >= 19 and y >= 9:
-        rows.append(board[y - 2][x - 4] + board[y - 4][x - 8] +
-                    board[y - 6][x - 12] + board[y - 8][x - 16])
-    if x <= 43 and y >= 9:
-        rows.append(board[y - 2][x + 4] + board[y - 4][x + 8] +
-                    board[y - 6][x + 8] + board[y - 8][x + 16])
-    if x >= 19 and y <= 21:
-        rows.append(board[y + 2][x - 4] + board[y + 4][x - 8] +
-                    board[y + 6][x - 8] + board[y + 8][x - 16])
-    if x <= 43 and y <= 21:
-        rows.append(board[y + 2][x + 4] + board[y + 4][x + 8] +
-                    board[y + 6][x + 8] + board[y + 8][x + 16])
+    if (x >= 13):
+        rows.append(board[y][x - 2] + board[y][x - 4] +
+                    board[y][x - 6] + board[y][x - 8])
+    if (x <= 25):
+        rows.append(board[y][x + 2] + board[y][x + 4] +
+                    board[y][x + 6] + board[y][x + 8])
+    if (y >= 5):
+        rows.append(board[y - 1][x] + board[y - 2][x] +
+                    board[y - 3][x] + board[y - 4][x])
+    if (y <= 11):
+        rows.append(board[y + 1][x] + board[y + 2][x] +
+                    board[y + 3][x] + board[y + 4][x])
+    if (x >= 13 and y >= 5):
+        rows.append(board[y - 1][x - 2] + board[y - 2][x - 4] +
+                    board[y - 3][x - 6] + board[y - 4][x - 8])
+    if (x <= 25 and y >= 5):
+        rows.append(board[y - 1][x + 2] + board[y - 2][x + 4] +
+                    board[y - 3][x + 6] + board[y - 4][x + 8])
+    if (x >= 13 and y <= 11):
+        rows.append(board[y + 1][x - 2] + board[y + 2][x - 4] +
+                    board[y + 3][x - 6] + board[y + 4][x - 8])
+    if (x <= 25 and y <= 11):
+        rows.append(board[y + 1][x + 2] + board[y + 2][x + 4] +
+                    board[y + 3][x + 6] + board[y + 4][x + 8])
     return rows
 
 
 def judge(rows: list, black: bool) -> bool:
-    if black:
+    if (black):
         chess = "@" * 4
     else:
-        chess = "#" * 4
+        chess = "O" * 4
     for row in rows:
         if (row == chess):
             if (black):
@@ -103,7 +100,7 @@ def judge(rows: list, black: bool) -> bool:
 
 
 def gameOver(board: list, handle: bool, x: int, y: int) -> bool:
-    if x != -1:
+    if (x != -1):
         rows = getRows(x, y, board)
         return judge(rows, handle)
     else:
@@ -111,13 +108,13 @@ def gameOver(board: list, handle: bool, x: int, y: int) -> bool:
 
 
 if __name__ == "__main__":
-    while True:
+    while (True):
         print("五子棋")
         bd = drawChessboard()
         displayChessboard(bd)
         black = False
         x, y = -1, -1
-        while not gameOver(bd, black, x, y):
+        while (not gameOver(bd, black, x, y)):
             black = not black
             bd, x, y = move(bd, black)
             displayChessboard(bd)
